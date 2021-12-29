e0204b7f5efbbfbb5516cee67ce3e9788b3b9a4f
pengewncong <185064212@qq.com>
2021-12-29 17:25:57 +0800

    redis

--- src/controller/video.go
+++ src/controller/video.go
@@ -1,9 +1,23 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go/server"
 )
 
 func VideoStream(c *gin.Context){
 	c.HTML(200,"video.html",gin.H{})
+}
+
+func RedisOperate(c *gin.Context){
+	result, err := server.Redis.Set("peng","wenport",0).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+
+	result, err = server.Redis.Get("peng").Result()
+	fmt.Println(result)
 }
