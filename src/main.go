09a11dd619e3b385e7cd0fd2bc0ed9e2e7615fcc
pengewncong <185064212@qq.com>
2021-12-29 15:22:09 +0800

    signal.notify

--- src/main.go
+++ src/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"go/router"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-
 
 	engine := gin.Default()
 	engine.LoadHTMLGlob("views/*")
@@ -15,5 +19,21 @@ func main() {
 	//禁用控制台颜色
 	router.Init(engine)
 
-	engine.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	hs := &http.Server{
+		Addr:"8080",
+		Handler:engine,
+	}
+	go hs.ListenAndServe() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	restart(hs)
+}
+
+func restart(hs *http.Server){
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	select {
+	case <- sigs:
+		hs.Shutdown(context.TODO())
+	}
 }
